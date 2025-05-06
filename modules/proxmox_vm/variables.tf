12634473f833bb068050ef6ce27309907fc10b0b
Wicaksono Trihatmaja <trihatmaja@gmail.com>
2025-05-06 14:24:38 +0700

    fixing variables

--- modules/proxmox_vm/variables.tf
+++ modules/proxmox_vm/variables.tf
@@ -7,8 +7,8 @@ variable "vm_tags" {
   type    = list(string)
   default = []
 }
-var.vm_username {}
-var.vm_password {}
+variable "vm_username" {}
+variable "vm_password" {}
 
 # Optional defaults
 variable "vm_cores"     { default = 2 }
